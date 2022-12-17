4c761be608236cffb573b120a62e2ebb9ca0abec
manaskalra62 <manaskalra62@gmail.com>
2022-12-17 17:49:13 +0530

    third
    Signed-off-by: manaskalra62 <manaskalra62@gmail.com>

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,35 +8,37 @@ const Cards = () => {
     <div className='w-full py-[10rem] px-4 bg-white'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
+              {/* <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" /> */}
+            
+              <h2 className='text-2xl font-bold text-center py-4'>Bench Mark</h2>
+              <p className='text-center text-4xl font-bold'>1,94,200</p>
               <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                  <p className='py-2 border-b mx-8 mt-8'>CPU: 67761 </p>
+                  <p className='py-2 border-b mx-8'>GPU:32801</p>
+                  <p className='py-2 border-b mx-8'>MEM:37712</p>
+                  <p className='py-2 border-b mx-8'>UX:55926</p>
               </div>
               <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
           </div>
           <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
+              {/* <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" /> */}
+              <h2 className='text-2xl font-bold text-center py-8'>Android Version</h2>
+              <p className='text-center text-4xl font-bold'>12</p>
               <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                  <p className='py-2 border-b mx-8 mt-8'>Security Update: June 5,2022 </p>
+                  <p className='py-2 border-b mx-8'>Google Play System Update: <span className='font -weight:400'>12</span></p>
+                  
               </div>
               <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
           </div>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+              {/* <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" /> */}
+              <h2 className='text-2xl font-bold text-center py-8'>Details</h2>
               <p className='text-center text-4xl font-bold'>$149</p>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
                   <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                 
               </div>
               <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
           </div>
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,32 +2,20 @@ import React from 'react';
 
 const Newsletter = () => {
   return (
-    <div className='w-full py-16 text-white px-4'>
-      <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
-        <div className='lg:col-span-2 my-4'>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
-            Want tips & tricks to optimize your flow?
-          </h1>
-          <p>Sign up to our newsletter and stay up to date.</p>
-        </div>
-        <div className='my-4'>
-          <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
-            <input
-              className='p-3 flex w-full rounded-md text-black'
-              type='email'
-              placeholder='Enter Email'
-            />
-            <button className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>
-              Notify Me
-            </button>
-          </div>
-          <p>
-            We care bout the protection of your data. Read our{' '}
-            <span className='text-[#00df9a]'>Privacy Policy.</span>
-          </p>
-        </div>
+    <div className=' bg-[#202626]'>
+     <div className='max-w-[1000px] mx-auto  grid md:grid-cols-2'>
+      <div className='flex flex-col justify-center space-y-1'>
+      <h1 className='text-white md:text-4xl sm-text-3xl font-bold py-2 mt-[300px]'>SECURITY</h1>
+      <p className='text-white text- 2.125rem;'>Your data, your rules. Along with monthly security updates to every supported device, we enhance existing privacy touchpoints around the OS and keep you informed of how the system shares your data.</p>
+     </div>
       </div>
+    <div className="right">
+      <img className=" w-60 ml-auto max-w-lg h-auto "src="https://lineageos.org/assets/img/security.png " alt="" />
+     
     </div>
+    </div>
+                    
+         
   );
 };
 
