3b5355670156ef234528fdd609934dc2ba1f6876
unknown <rishitadube2003@gmail.com>
2022-12-17 20:31:05 +0530

    added business

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -25,7 +25,7 @@ const Cards = () => {
               <h2 className='text-2xl font-bold text-center py-8'>Android Version</h2>
               <p className='text-center text-4xl font-bold'>12</p>
               <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>Security Update: June 5,2022 </p>
+                  <p className='py-2 border-b mx-8 mt-8'>Security Update: July 5,2022 </p>
                   <p className='py-2 border-b mx-8'>Google Play System Update: <span className='font -weight:400'>12</span></p>
                   
               </div>
@@ -33,11 +33,12 @@ const Cards = () => {
           </div>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               {/* <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" /> */}
-              <h2 className='text-2xl font-bold text-center py-8'>Details</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
-              <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
+              <h2 className='text-2xl font-bold text-center py-8'>Business Models</h2>
+              <p className='text-center text-4xl font-bold'>3</p>
+              <div className='text-center font-medium mt-[10px]'>
+                  <p className='py-2 border-b mx-8 mt-8'>Device-based model</p>
+                  <p className='py-2 border-b mx-8'>Service-based model</p>
+                  <p className='py-2 border-b mx-8'>Freemium model</p>
                  
               </div>
               <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
